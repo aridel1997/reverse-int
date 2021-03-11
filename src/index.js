5b3fb0b7172fa12aa528828ee99591b03e8f6e77
aridel1997 <arinadelendik1997@gmail.com>
2021-03-11 17:59:28 +0300

    feat: add new algorithms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        return reverseNumber(-n);
+    } else {
+        return reverseNumber(n);
+    }
+    function reverseNumber(n) {
+        let array = String(n).split("");
+        let str = +array.reverse().join("");
+        return (str)
+    }
 }
